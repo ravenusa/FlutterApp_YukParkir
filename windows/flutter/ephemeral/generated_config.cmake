1c43a6179cb0c288e6a6f24ee7cb2d2edfde7c15
ravenusa <80948124+ravenusa@users.noreply.github.com>
2023-01-15 01:07:32 +0700

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "G:\\flutter_windows_3.3.10-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=G:\\flutter_windows_3.3.10-stable\\flutter"
+  "PROJECT_DIR=F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir"
+  "FLUTTER_ROOT=G:\\flutter_windows_3.3.10-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir"
+  "FLUTTER_TARGET=F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=F:\\OneDrive - UNIVERSITAS AMIKOM YOGYAKARTA\\SEMESTER 5\\RPL\\FP\\yukparkir\\yukparkir\\.dart_tool\\package_config.json"
+)
